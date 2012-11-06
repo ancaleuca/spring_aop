c4c907da67b0f5e5dd3394f5a25c3d29382d0b6f
ancaleuca <ancaleuca2005@gmail.com>
2012-11-06 01:09:04 +0000

    Added example for annotations

--- workspace/foo/src/main/java/com/example/LogMe.java
+++ workspace/foo/src/main/java/com/example/LogMe.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class LogMe {
 	
+	@Pointcut("execution(* com.example.Foo.doSomething(..))")
+	public void something() {
+		//Dummy method!
+	}
+	
+	@Before("something()")
 	public void logBefore() {
 		System.out.println("LogMe is logging before");
 	}
--- workspace/foo/src/main/java/com/example/Main.java
+++ workspace/foo/src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	
 	public static void main(String[] args) {
+		// When next line of code runs, the proxy class for weaving in the logging aspect is created as well.
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
 		Foo foo = (Foo) applicationContext.getBean("foo");
 		foo.doSomething();
